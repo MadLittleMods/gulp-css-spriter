8e8fc99d6ba326090f69bc264921c4effeef4641
Eric Eastwood <contact@ericeastwood.com>
2015-01-06 17:29:56 -0600

    Initial code commit

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
-gulp-css-spriter
-================
+# gulp-css-spriter
 
-Sprite Generation from CSS source files. The best and different approach to sprite sheets.
+`gulp-css-spriter` looks through the CSS you pass it to gather all of the background images used. It then creates a sprite sheet and updates the references in the CSS.
+
+You can easily exclude/include certain background image declarations using meta info in your styles(see below) and `includeMode` option(see options section below).
+
+# About
+
+`gulp-css-spriter` uses [spritesmith](https://www.npmjs.com/package/spritesmith) behind the scenes for creating the sprite sheet.
+
+
+# Usage
+
+## Basic usage
+
+This is most likely the setup you will probably end up using.
+
+```
+gulp.task('css', function() {
+	return gulp.src('./src/css/styles.css')
+		.pipe(spriter({
+			// The path and file name of where we will save the sprite sheet
+			'spriteSheet': './dist/images/spritesheet.png',
+			// Because we don't know where you will end up saving the CSS file at this point in the pipe,
+			// we need a litle help identifying where it will be.
+			'pathToSpriteSheetFromCSS': '../images/spritesheet.png'
+		}))
+		.pipe(gulp.dest('./dist/css'));
+});
+```
+
+## Barebones usage
+
+The slimmest usage possible.
+
+```
+gulp.task('css', function() {
+	return gulp.src('./styles.css')
+		.pipe(spriter())
+		.pipe(gulp.dest('./'));
+});
+```
+
+
+# Options
+
+ - `options`: object - hash of options
+ 	 - `includeMode`: string - Determines whether meta data is necessary or not
+ 	 	 - Values: 'implicit', 'explicit'
+ 	 	 - Default: 'implicit'
+ 	 	 - For example, if `explicit`, you must have meta `include` as `true` in order for the image declarations to be included in the spritesheet: `/* @meta {"spritesheet": {"include": true}} */`
+ 	 	 - If left default at `implicit`, all images will be included in the spritesheet; except for image declarations with meta `include` as `false`: `/* @meta {"spritesheet": {"include": false}} */`
+ 	 - `spriteSheet`: string - The path and file name of where we will save the sprite sheet
+ 	 	 - Default: 'spritesheet.png'
+	 - `pathToSpriteSheetFromCSS`: string - Because we don't know where you will end up saving the CSS file at this point in the pipe, we need a litle help identifying where it will be. We will use this as the reference to the sprite sheet image in the CSS piped in.
+	 	 - Default: 'spritesheet.png'
+	 - `spriteSheetBuildCallback`: function - Same as the [spritesmith callback](https://www.npmjs.com/package/spritesmith#-spritesmith-params-callback-)
+	 	 - Default: null
+	 	 - Callback has a parameters as so: `function(err, result)`
+	 	 	 - `result.image`: Binary string representation of image
+	 	 	 - `result.coordinates`: Object mapping filename to {x, y, width, height} of image
+	 	 	 - `result.properties`: Object with metadata about spritesheet {width, height}
+	 - `spritesmithOptions`: object - Any option you pass in here, will be passed through to spritesmith. [See spritesmith options documenation](https://www.npmjs.com/package/spritesmith#-spritesmith-params-callback-)
+	 	 - Default: {}
---
+++ index.js
@@ -0,0 +1,173 @@
+
+// Meta info looks like: `/* @meta {"spritesheet": {"include": false}} */`
+
+var fs = require('fs-extra');
+var path = require('path');
+var through = require('through2'); // https://www.npmjs.org/package/through2
+var extend = require('extend'); // https://www.npmjs.org/package/extend
+var gutil = require('gulp-util'); // https://www.npmjs.org/package/gulp-util
+
+var css = require('css'); // https://www.npmjs.com/package/css
+var spritesmith = require('spritesmith'); // https://www.npmjs.com/package/spritesmith
+
+
+var spriterUtil = require('./lib/spriter-util');
+var getAllBackgroundImageDeclarations = require('./lib/get-all-background-image-declarations');
+var transformChunksWithSpriteSheetData = require('./lib/transform-chunks-with-sprite-sheet-data');
+
+
+
+
+
+// consts
+const PLUGIN_NAME = 'gulp-css-spriter';
+
+
+var spriter = function(options) {
+
+	var defaults = {
+		// ('implicit'|'explicit')
+		'includeMode': 'implicit',
+		// The path and file name of where we will save the sprite sheet
+		'spriteSheet': 'spritesheet.png',
+		// Because we don't know where you will end up saving the CSS file at this point in the pipe,
+		// we need a litle help identifying where it will be.
+		'pathToSpriteSheetFromCSS': 'spritesheet.png',
+		// Same as the spritesmith callback `function(err, result)`
+		// result.image: Binary string representation of image
+		// result.coordinates: Object mapping filename to {x, y, width, height} of image
+		// result.properties: Object with metadata about spritesheet {width, height}
+		'spriteSheetBuildCallback': null,
+		// Any option you pass in here, will be passed through to spritesmith
+		// https://www.npmjs.com/package/spritesmith#-spritesmith-params-callback-
+		'spritesmithOptions': {}
+	};
+
+	var settings = extend({}, defaults, options);
+
+	// This is where we keep track of the declarations for each chunk and 
+	// info on the chunk itself
+	var chunkDataList = [];
+	// Since we only want a unique list of images, use a object
+	var imageList = {};
+
+	var stream = through.obj(function(chunk, enc, cb) {
+		// http://nodejs.org/docs/latest/api/stream.html#stream_transform_transform_chunk_encoding_callback
+		//console.log('transform');
+
+		// Each `chunk` is a vinyl file: https://www.npmjs.com/package/vinyl
+		// chunk.cwd
+		// chunk.base
+		// chunk.path
+		// chunk.contents
+
+
+		if (chunk.isStream()) {
+			throw new gutil.PluginError(PLUGIN_NAME, 'Cannot operate on stream');
+		}
+		else if (chunk.isBuffer()) {
+			var contents = String(chunk.contents);
+
+			var styles = css.parse(contents, {
+				'silent': false
+			});
+
+			var chunkBackgroundImageDeclarations = getAllBackgroundImageDeclarations(styles);
+
+			// Filter out any declartion with the `include: false` meta
+			chunkBackgroundImageDeclarations = chunkBackgroundImageDeclarations.filter(function(declaration) {
+				var metaIncludeValue = (declaration.meta && declaration.meta.spritesheet && declaration.meta.spritesheet.include);
+				var shouldIncludeBecauseImplicit = settings.includeMode == 'implicit' && (metaIncludeValue === undefined || metaIncludeValue);
+				var shouldIncludeBecauseExplicit = settings.includeMode == 'explicit' && metaIncludeValue !== undefined && metaIncludeValue;
+				var shouldInclude = shouldIncludeBecauseImplicit || shouldIncludeBecauseExplicit;
+
+				// Only return declartions that shouldn't be skipped
+				return shouldInclude;
+			});
+
+
+			// Get a list of all of the images
+			var backgroundURLMatchAllRegex = new RegExp(spriterUtil.backgroundURLRegex.source, "gi");
+			chunkBackgroundImageDeclarations.forEach(function(declaration) {
+				var backgroundImageMatch;
+				while((backgroundImageMatch = backgroundURLMatchAllRegex.exec(declaration.value)) != null) {
+					// javascript RegExp has a bug when the match has length 0
+					if (backgroundImageMatch.index === backgroundURLMatchAllRegex.lastIndex) {
+						// Avoid infinite recursion
+						backgroundURLMatchAllRegex.lastIndex += 1;
+					}
+					// the match variable is an array that contains the matching groups
+					//console.log(backgroundImageMatch[1]);
+					imageList[path.join(path.dirname(chunk.path), backgroundImageMatch[2])] = true;
+				}
+			});
+
+
+
+			// Keep track of each chunk and what declarations go with it
+			// Because the positions/line numbers pertain to that chunk only
+			chunkDataList.push({
+				'vinylFile': chunk,
+				'backgroundImageDeclarations': chunkBackgroundImageDeclarations
+			});
+
+		}
+
+
+		return cb();
+
+	}, function(cb) {
+		// http://nodejs.org/docs/latest/api/stream.html#stream_transform_flush_callback
+		//console.log('flush');
+		var self = this;
+
+		// Generate the spritesheet
+		var spritesmithOptions = extend({}, settings.spritesmithOptions, { src: Object.keys(imageList) });
+		spritesmith(spritesmithOptions, function handleResult(err, result) {
+
+			// If there was an error, throw it
+			if (err) {
+				throw new gutil.PluginError(PLUGIN_NAME, 'Error creating sprite sheet image:', err);
+			}
+
+			// Save out the spritesheet image
+			if(settings.spriteSheet) {
+				fs.outputFile(settings.spriteSheet, result.image, 'binary', function(err) {
+					if(err) {
+						throw new gutil.PluginError(PLUGIN_NAME, 'Spritesheet failed to save:', err);
+					} else {
+						//console.log("The file was saved!");
+
+						// Push all of the chunks back on the pipe
+						var transformedChunkDataList = transformChunksWithSpriteSheetData(chunkDataList, result.coordinates, settings.pathToSpriteSheetFromCSS);
+						transformedChunkDataList.forEach(function(chunkData) {
+							self.push(chunkData.vinylFile);
+						});
+
+
+						// Call a callback from the settings the user can hook onto
+						if(settings.spriteSheetBuildCallback) {
+							settings.spriteSheetBuildCallback(err, result);
+						}
+
+						// "call callback when the flush operation is complete."
+						cb();
+					}
+				});
+			}
+
+			
+		});
+
+
+
+		
+
+	});
+
+	// returning the file stream
+	return stream;
+};
+
+
+module.exports = spriter;
